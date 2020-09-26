0fa39641cadae40130c3abd4d0c23510b2de8ee6
Ahmet Yusuf Basaran <aybasaran10@gmail.com>
2020-09-26 03:28:30 +0300

    Initial Commit

---
+++ Dicee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee
+//
+//  Created by Ahmet Yusuf Basaran on 26.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Ahmet Yusuf Basaran on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
